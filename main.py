d16fa695d9588e1705744ef89c15c8067fadc49b
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2025-01-10 14:56:29 -0600

    Added first menu option

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+print("1.- Suma de n cantidad de números (positivos y negativos")
 option = input("Seleccione una opción: ")
+
+inputNumber = ""
+sum = 0
+
+match(option):
+    case "1":
+        while inputNumber != 0:
+            inputNumber = int(input("Número a sumar (0 para finalizar): "))
+            sum += inputNumber
+        print(f"La suma es {sum}")
+
+
+
