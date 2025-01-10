bd76d11974caee697e180835b7e25611dffbab01
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2025-01-10 15:47:40 -0600

    Added while loops for repeating operations and options

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-print("1.- Suma de n cantidad de números (positivos y negativos")
-print("4.- Calcular el factorial (!n) de un número")
-option = input("Seleccione una opción: ")
+execute = True
 
-match option:
-    case "1":
-        inputNumber = ""
-        sum = 0
-        while inputNumber != 0:
-            inputNumber = int(input("Número a sumar (0 para finalizar): "))
-            sum += inputNumber
-        print(f"La suma es {sum}")
-    case "4":
-        inputNumber = int(input("Inserte un número para calcular su factorial: "))
-        if inputNumber == 0:
-            print(f"El factorial de {inputNumber} es 1")
-        elif inputNumber > 0:
-            count = inputNumber
-            factorial = 1
-            while count > 0:
-                factorial = factorial * count
-                count -= 1
-            print(f"El factorial de {inputNumber} es {factorial}")
+while execute:
+    print("1.- Suma de n cantidad de números (positivos y negativos")
+    print("4.- Calcular el factorial (!n) de un número")
+
+    option = input("Seleccione una opción: ")
+
+    match option:
+        case "1":
+            repeat = True
+            while repeat:
+                inputNumber = ""
+                sum = 0
+
+                while inputNumber != 0:
+                    inputNumber = int(input("Número a sumar (0 para finalizar): "))
+                    sum += inputNumber
+                print(f"La suma es {sum}")
+                repeat = input("¿Desea realizar otra suma? (s/n): ") == "s"
+        case "4":
+            repeat = True
+            while repeat:
+                inputNumber = int(input("Inserte un número para calcular su factorial: "))
+                if inputNumber == 0:
+                    print(f"El factorial de {inputNumber} es 1")
+                elif inputNumber > 0:
+                    count = inputNumber
+                    factorial = 1
+                    while count > 0:
+                        factorial = factorial * count
+                        count -= 1
+                    print(f"El factorial de {inputNumber} es {factorial}")
+                repeat = input("¿Desea calcular el factorial de otro número? (s/n): ") == "s"
+
+    execute = input("¿Desea elegir alguna otra opción del menú? (s/n): ") == "s"
 
 
 
