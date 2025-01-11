15f4e20d41e9a70a28b89ca021e72fd47a257d9f
Adrian-De-Leon <adrian.leon1814@gmail.com>
2025-01-10 21:06:31 -0600

    Added 7 menu option (Average)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ execute = True
 while execute:
     print("1.- Suma de n cantidad de números (positivos y negativos")
     print("4.- Calcular el factorial (!n) de un número")
+    print("7.- Promedio de una serie de números")
 
     option = input("Seleccione una opción: ")
 
@@ -32,7 +33,24 @@ while execute:
                         count -= 1
                     print(f"El factorial de {inputNumber} es {factorial}")
                 repeat = input("¿Desea calcular el factorial de otro número? (s/n): ") == "s"
-
+        case "7":
+            repeat = True
+            while repeat:
+                dividend = 0
+                divisor = 0
+                print("Introduce números para calcular el promedio. Ingresa -1 para terminar")
+                while True:
+                    number = float(input("Número: "))
+                    if number == 1:
+                        break
+                    dividend += number
+                    divisor += 1
+                if divisor > 0:
+                    average = dividend / divisor
+                    print("El promedio es: ", average)
+                else:
+                    print("No se ingresaron números válidos para calcular el promedio")
+                repeat = input("¿Deseas calcular el promedio de otra serie? (s/n): ") == "s"
     execute = input("¿Desea elegir alguna otra opción del menú? (s/n): ") == "s"
 
 
