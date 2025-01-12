b07e493d46322924efd2f19731abc75165c27ffc
Yahir Sias <yahirsias77@gmail.com>
2025-01-11 22:26:03 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 execute = True
 
 while execute:
+    print("\n ")
     print("1.- Suma de n cantidad de números (positivos y negativos")
+    print("2.- Producto entre 'n' numeros")
     print("4.- Calcular el factorial (!n) de un número")
+    print("5.- Impresión en la pantalla de las tablas de multiplicar entre el 1 al 10.")
     print("7.- Promedio de una serie de números")
     print("8.- Buscar valores máximo y mínimo de una serie de números")
 
@@ -20,6 +23,23 @@ while execute:
                     sum += inputNumber
                 print(f"La suma es {sum}")
                 repeat = input("¿Desea realizar otra suma? (s/n): ") == "s"
+
+        case "2":
+            repeat = True
+            while repeat:
+                inputNumber = ""
+                mult = 1
+                multiplo = 0
+                anterior = 0
+
+                while inputNumber != 0:
+                    inputNumber = int(input("Número a multiplicar (0 para finalizar), la serie iniciara con un 1: "))
+                    valor = inputNumber
+                    anterior = mult
+                    mult = mult * inputNumber
+                    print("La multiplicacion de ", anterior, "multiplicado por:", valor, "da como resultado: ", mult)
+                repeat = input("¿Desea realizar otra multiplicacion? (s/n): ") == "s"
+
         case "4":
             repeat = True
             while repeat:
@@ -34,6 +54,20 @@ while execute:
                         count -= 1
                     print(f"El factorial de {inputNumber} es {factorial}")
                 repeat = input("¿Desea calcular el factorial de otro número? (s/n): ") == "s"
+
+        case "5":
+            repeat = True
+            while repeat:
+                opcion = int(input("Selecciona la tabla de multiplicar que quieras mostrar, entre el 1 al 10: "))
+                if opcion >= 1 and opcion <= 10:
+                    contador = int(0)
+                    for contador in range(10):
+                        contador += 1
+                        print("El numero", opcion, "por", contador, "es igual a", (opcion * contador))
+                else:
+                    print("Opcion equivocada")
+                repeat = input("¿Desea ver la tabla de otro número? (s/n): ") == "s"
+
         case "7":
             repeat = True
             while repeat:
@@ -52,6 +86,7 @@ while execute:
                 else:
                     print("No se ingresaron números válidos para calcular el promedio")
                 repeat = input("¿Deseas calcular el promedio de otra serie? (s/n): ") == "s"
+
         case "8":
             repeat = True
             while repeat:
